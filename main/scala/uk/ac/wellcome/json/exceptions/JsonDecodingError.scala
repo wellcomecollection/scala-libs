d2cb554145936eaccce2f83e1df53c4b41c14ef8
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2018-07-31 11:00:32 +0000

    Apply auto-formatting rules

--- main/scala/uk/ac/wellcome/json/exceptions/JsonDecodingError.scala
+++ main/scala/uk/ac/wellcome/json/exceptions/JsonDecodingError.scala
@@ -1,4 +1,3 @@
 package uk.ac.wellcome.json.exceptions
 
-case class JsonDecodingError(e: Throwable)
-  extends Exception(e.getMessage)
+case class JsonDecodingError(e: Throwable) extends Exception(e.getMessage)
