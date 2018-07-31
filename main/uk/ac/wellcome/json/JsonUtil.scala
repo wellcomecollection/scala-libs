2ff0b2a4c58fab63528ae22ab25f40bb4b027e33
Alex Chan <a.chan@wellcome.ac.uk>
2018-07-31 11:39:38 +0100

    Initial commit

---
+++ main/uk/ac/wellcome/json/JsonUtil.scala
@@ -0,0 +1,36 @@
+package uk.ac.wellcome.json
+
+import grizzled.slf4j.Logging
+import io.circe.generic.extras.{AutoDerivation, Configuration}
+import io.circe.java8.time.TimeInstances
+import io.circe.parser._
+import io.circe.syntax._
+import io.circe.{Decoder, Encoder, Json}
+
+import scala.util.Try
+
+object JsonUtil extends AutoDerivation with TimeInstances with Logging {
+
+  implicit val customConfig: Configuration =
+    Configuration.default.withDefaults
+      .withDiscriminator("type")
+
+  def toJson[T](value: T)(implicit encoder: Encoder[T]): Try[String] = {
+    assert(encoder != null)
+    Try(value.asJson.noSpaces)
+  }
+
+  def toMap[T](json: String)(
+    implicit decoder: Decoder[T]): Try[Map[String, T]] = {
+    assert(decoder != null)
+    fromJson[Map[String, T]](json)
+  }
+
+  def fromJson(json: String): Try[Json] =
+    parse(json).toTry
+
+  def fromJson[T](json: String)(implicit decoder: Decoder[T]): Try[T] = {
+    assert(decoder != null)
+    decode[T](json).toTry
+  }
+}
---
+++ test/scala/uk/ac/wellcome/json/utils/JsonAssertions.scala
@@ -0,0 +1,19 @@
+package uk.ac.wellcome.json.utils
+
+import org.scalatest.Matchers
+import io.circe.parser._
+
+trait JsonAssertions extends Matchers {
+
+  def assertJsonStringsAreEqual(json1: String, json2: String) = {
+    val tree1 = parse(json1).right.get
+    val tree2 = parse(json2).right.get
+    tree1 shouldBe tree2
+  }
+
+  def assertJsonStringsAreDifferent(json1: String, json2: String) = {
+    val tree1 = parse(json1).right.get
+    val tree2 = parse(json2).right.get
+    tree1 shouldNot be(tree2)
+  }
+}
