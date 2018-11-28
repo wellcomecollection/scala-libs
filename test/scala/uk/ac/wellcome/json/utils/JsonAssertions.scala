fe19470f0064e15cdc1d21ba6cd12a342c58029e
Alex Chan <a.chan@wellcome.ac.uk>
2018-11-28 10:13:58 +0000

    Ensure you can see why parsing failed if/when it fails

--- test/scala/uk/ac/wellcome/json/utils/JsonAssertions.scala
+++ test/scala/uk/ac/wellcome/json/utils/JsonAssertions.scala
@@ -1,19 +1,26 @@
 package uk.ac.wellcome.json.utils
 
-import org.scalatest.Matchers
+import io.circe.Json
+import org.scalatest.{Assertion, Matchers}
 import io.circe.parser._
 
 trait JsonAssertions extends Matchers {
 
-  def assertJsonStringsAreEqual(json1: String, json2: String) = {
-    val tree1 = parse(json1).right.get
-    val tree2 = parse(json2).right.get
+  def assertJsonStringsAreEqual(json1: String, json2: String): Assertion = {
+    val tree1 = parseOrElse(json1)
+    val tree2 = parseOrElse(json2)
     tree1 shouldBe tree2
   }
 
-  def assertJsonStringsAreDifferent(json1: String, json2: String) = {
-    val tree1 = parse(json1).right.get
-    val tree2 = parse(json2).right.get
+  def assertJsonStringsAreDifferent(json1: String, json2: String): Assertion = {
+    val tree1 = parseOrElse(json1)
+    val tree2 = parseOrElse(json2)
     tree1 shouldNot be(tree2)
   }
+
+  private def parseOrElse(jsonString: String): Json =
+    parse(jsonString) match {
+      case Right(t) => t
+      case Left(err) => throw err
+    }
 }
