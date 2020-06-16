17bd4e4fa6e20d459a8a142671f11c8d0813a25f
Robert Kenny <r.kenny@wellcome.ac.uk>
2020-06-16 11:49:40 +0100

    compile

--- storage/src/main/scala/uk/ac/wellcome/storage/store/azure/AzureStreamReadable.scala
+++ storage/src/main/scala/uk/ac/wellcome/storage/store/azure/AzureStreamReadable.scala
@@ -7,8 +7,6 @@ import grizzled.slf4j.Logging
 import uk.ac.wellcome.storage._
 import uk.ac.wellcome.storage.store.Readable
 import uk.ac.wellcome.storage.streaming.InputStreamWithLength
-
-import scala.collection.JavaConverters._
 import scala.util.{Failure, Success, Try}
 
 trait AzureStreamReadable extends Readable[ObjectLocation, InputStreamWithLength] with Logging {
@@ -41,17 +39,6 @@ trait AzureStreamReadable extends Readable[ObjectLocation, InputStreamWithLength
   }
 
   private def buildInputStream(blobInputStream: BlobInputStream): InputStreamWithLength = {
-    // We get a mutable.Map from the S3 SDK, but we want an immutable Map to pass
-    // out to the codebase, hence this slightly odd construction!
-    val userMetadata = blobInputStream.getProperties.getMetadata
-    val metadata = userMetadata
-      .keySet()
-      .asScala
-      .map { k =>
-        (k, userMetadata.get(k))
-      }
-      .toMap
-
     new InputStreamWithLength(
       blobInputStream,
       length = blobInputStream.getProperties.getBlobSize,
