40be04edbfe6b476d82ce97375bdc63f7cf97233
Robert Kenny <r.kenny@wellcome.ac.uk>
2020-06-17 14:35:56 +0100

    start adding tests

---
+++ storage/src/test/scala/uk/ac/wellcome/storage/tags/azure/AzureBlobMetadataTest.scala
@@ -0,0 +1,55 @@
+package uk.ac.wellcome.storage.tags.azure
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import uk.ac.wellcome.fixtures.TestWith
+import uk.ac.wellcome.storage.ObjectLocation
+import uk.ac.wellcome.storage.fixtures.AzureFixtures
+import uk.ac.wellcome.storage.fixtures.AzureFixtures.Container
+import uk.ac.wellcome.storage.tags.{Tags, TagsTestCases}
+import scala.collection.JavaConverters._
+
+
+class AzureBlobMetadataTest
+  extends AnyFunSpec
+    with Matchers
+    with TagsTestCases[ObjectLocation, Container]
+    with AzureFixtures {
+
+  def putObject(location: ObjectLocation, metadata: Map[String, String] = Map.empty) = {
+    val streamLength = 256
+    val allowOverwrites = true
+    val inputStream = randomInputStream(length = streamLength)
+
+    val individualBlobClient =
+      azureClient
+        .getBlobContainerClient(location.namespace)
+        .getBlobClient(location.path)
+
+    individualBlobClient.upload(
+      inputStream,
+      streamLength,
+      allowOverwrites,
+    )
+
+    individualBlobClient
+      .setMetadata(metadata.asJava)
+  }
+
+  // TODO: TagsTestCases needs to handle Azure metadata
+  override def withTags[R](initialTags: Map[ObjectLocation, Map[String, String]])(testWith: TestWith[Tags[ObjectLocation], R]): R = {
+    initialTags
+      .foreach { case (location, tags) =>
+        putObject(
+          location = location,
+          metadata = tags
+        )
+      }
+
+  }
+
+  override def createIdent(context: Container): ObjectLocation = ???
+
+  override def withContext[R](testWith: TestWith[Container, R]): R = ???
+
+}
