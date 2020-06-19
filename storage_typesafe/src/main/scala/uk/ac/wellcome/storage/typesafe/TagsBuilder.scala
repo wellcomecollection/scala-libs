81ef37ef2a1785cb2758d07e3a79f477c2238cea
Robert Kenny <r.kenny@wellcome.ac.uk>
2020-06-19 13:03:25 +0100

    Add tag-builder

---
+++ storage_typesafe/src/main/scala/uk/ac/wellcome/storage/typesafe/TagsBuilder.scala
@@ -0,0 +1,52 @@
+package uk.ac.wellcome.storage.typesafe
+
+import com.azure.storage.blob.{BlobServiceClient, BlobServiceClientBuilder}
+import com.typesafe.config.Config
+import uk.ac.wellcome.storage.ObjectLocation
+import uk.ac.wellcome.storage.store.RetryableReadable
+import uk.ac.wellcome.storage.tags.Tags
+import uk.ac.wellcome.storage.tags.azure.AzureBlobMetadata
+import uk.ac.wellcome.storage.tags.s3.S3Tags
+import uk.ac.wellcome.typesafe.config.builders.EnrichConfig._
+
+object TagsBuilder {
+  import CloudProvider._
+
+  //  ## Expects HOCON config like:
+  //  {
+  //    cloudProvider = "aws"
+  //    aws.s3.region = "eu-west-1"
+  //  }
+  //  ## OR
+  //  {
+  //    cloudProvider = "azure"
+  //    azure.blobStore.connectionString = "UseDevelopmentStorage=true;"
+  //  }
+  //
+
+  def buildClient(config: Config): Tags[ObjectLocation] with RetryableReadable[Map[String, String]] = {
+
+    val cloudProvider = config
+      .getStringOption("cloudProvider")
+      .flatMap(CloudProvider.create)
+      .getOrElse(AWS)
+
+    cloudProvider match {
+      case AWS => {
+        val s3Client = S3Builder.buildS3Client(config)
+        new S3Tags()(s3Client)
+      }
+      case Azure => {
+        val connectionString = config
+          .requireString("azure.blobStore.connectionString")
+
+        val azureClient: BlobServiceClient =
+          new BlobServiceClientBuilder()
+            .connectionString(connectionString)
+            .buildClient()
+
+        new AzureBlobMetadata()(azureClient)
+      }
+    }
+  }
+}
