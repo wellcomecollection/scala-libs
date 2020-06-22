69e4ef072466c09262765051dead97c04774e22c
Alex Chan <alex@alexwlchan.net>
2020-06-22 11:30:38 +0100

    Add some custom JSON decoders for old-style ObjectLocation

--- storage/src/main/scala/uk/ac/wellcome/storage/s3/S3ObjectLocation.scala
+++ storage/src/main/scala/uk/ac/wellcome/storage/s3/S3ObjectLocation.scala
@@ -2,6 +2,7 @@ package uk.ac.wellcome.storage.s3
 
 import java.nio.file.Paths
 
+import io.circe.{Decoder, DecodingFailure, HCursor}
 import uk.ac.wellcome.storage.{Location, Prefix}
 
 // Note: we use the old namespace/path naming convention because those are the
@@ -26,4 +27,46 @@ case class S3ObjectLocationPrefix(
 
   override def asLocation(parts: String*): S3ObjectLocation =
     S3ObjectLocation(namespace = namespace, path = path).join(parts: _*)
+}
+
+// Note: the precursor to these classes was ObjectLocation, which used the
+// fields "namespace/path" rather than provider-specific terms.
+//
+// These decoders allow you to read JSON that was encoded with the old code.
+
+trait S3Decodable {
+  def createDecoder[T](constructor: (String, String) => T): Decoder[T] =
+    (cursor: HCursor) => {
+      val oldStyle: Either[DecodingFailure, T] =
+        for {
+          bucket <- cursor.downField("namespace").as[String]
+          key <- cursor.downField("path").as[String]
+        } yield constructor(bucket, key)
+
+      val newStyle =
+        for {
+          bucket <- cursor.downField("bucket").as[String]
+          key <- cursor.downField("key").as[String]
+        } yield constructor(bucket, key)
+
+      (oldStyle, newStyle) match {
+        case (Right(location), _) => Right(location)
+        case (_, Right(location)) => Right(location)
+        case (_, Left(decodingFailure)) => Left(decodingFailure)
+      }
+    }
+}
+
+case object S3ObjectLocation extends S3Decodable {
+  implicit val decoder: Decoder[S3ObjectLocation] =
+    createDecoder[S3ObjectLocation] { (bucket: String, key: String) =>
+      S3ObjectLocation(namespace = bucket, path = key)
+    }
+}
+
+case object S3ObjectLocationPrefix extends S3Decodable {
+  implicit val decoder: Decoder[S3ObjectLocationPrefix] =
+    createDecoder[S3ObjectLocationPrefix] { (bucket: String, key: String) =>
+      S3ObjectLocationPrefix(namespace = bucket, path = key)
+    }
 }
--- storage/src/test/scala/uk/ac/wellcome/storage/s3/S3ObjectLocationTest.scala
+++ storage/src/test/scala/uk/ac/wellcome/storage/s3/S3ObjectLocationTest.scala
@@ -1,7 +1,9 @@
 package uk.ac.wellcome.storage.s3
 
+import uk.ac.wellcome.json.JsonUtil._
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
+import uk.ac.wellcome.storage.JsonDecodingError
 
 class S3ObjectLocationTest extends AnyFunSpec with Matchers {
   it("creates a location from a prefix") {
@@ -18,16 +20,67 @@ class S3ObjectLocationTest extends AnyFunSpec with Matchers {
     )
   }
 
-  it("decodes from an old-style ObjectLocation") {
-    import uk.ac.wellcome.json.JsonUtil._
+  describe("JSON decoding of Location") {
+    it("decodes from an old-style ObjectLocation") {
+      val jsonString = """{"namespace": "my-bucket", "path": "myfile.txt"}"""
 
-    val location = S3ObjectLocation(
-      namespace = "my-bucket",
-      path = "myfile.txt"
-    )
+      val location = fromJson[S3ObjectLocation](jsonString).get
 
-    println(toJson(location).get)
+      location shouldBe S3ObjectLocation(
+        namespace = "my-bucket",
+        path = "myfile.txt"
+      )
+    }
 
-    true shouldBe false
+    it("decodes from a new-style S3ObjectLocation") {
+      val jsonString = """{"bucket": "my-bucket", "key": "myfile.txt"}"""
+
+      val location = fromJson[S3ObjectLocation](jsonString).get
+
+      location shouldBe S3ObjectLocation(
+        namespace = "my-bucket",
+        path = "myfile.txt"
+      )
+    }
+
+    it("errors if the JSON is malformed") {
+      val jsonString = """{"name": "lexie"}"""
+
+      val result = fromJson[S3ObjectLocation](jsonString)
+
+      result.failed.get shouldBe a[JsonDecodingError]
+    }
+  }
+
+  describe("JSON decoding of Prefix") {
+    it("decodes from an old-style ObjectLocationPrefix") {
+      val jsonString = """{"namespace": "my-bucket", "path": "myfile.txt"}"""
+
+      val location = fromJson[S3ObjectLocationPrefix](jsonString).get
+
+      location shouldBe S3ObjectLocationPrefix(
+        namespace = "my-bucket",
+        path = "myfile.txt"
+      )
+    }
+
+    it("decodes from a new-style S3ObjectLocation") {
+      val jsonString = """{"bucket": "my-bucket", "key": "myfile.txt"}"""
+
+      val location = fromJson[S3ObjectLocationPrefix](jsonString).get
+
+      location shouldBe S3ObjectLocationPrefix(
+        namespace = "my-bucket",
+        path = "myfile.txt"
+      )
+    }
+
+    it("errors if the JSON is malformed") {
+      val jsonString = """{"name": "lexie"}"""
+
+      val result = fromJson[S3ObjectLocationPrefix](jsonString)
+
+      result.failed.get shouldBe a[JsonDecodingError]
+    }
   }
 }
