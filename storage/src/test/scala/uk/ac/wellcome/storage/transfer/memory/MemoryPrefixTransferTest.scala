01df3133fbc5e99952903a309384b8aeec1ccd99
Alex Chan <alex@alexwlchan.net>
2020-07-24 08:23:32 +0100

    Inline the old ObjectLocationPrefixTransfer trait

--- storage/src/test/scala/uk/ac/wellcome/storage/transfer/memory/MemoryPrefixTransferTest.scala
+++ storage/src/test/scala/uk/ac/wellcome/storage/transfer/memory/MemoryPrefixTransferTest.scala
@@ -64,11 +64,19 @@ class MemoryPrefixTransferTest
 
   class MemoryObjectLocationPrefixTransfer(initialEntries: Map[ObjectLocation, Record])
     extends MemoryStore[ObjectLocation, Record](initialEntries = initialEntries)
-      with MemoryPrefixTransfer[ObjectLocation, ObjectLocationPrefix, Record]
-      with ObjectLocationPrefixTransfer {
+      with MemoryPrefixTransfer[ObjectLocation, ObjectLocationPrefix, Record] {
     override protected def startsWith(location: ObjectLocation, prefix: ObjectLocationPrefix): Boolean = {
       location.namespace == prefix.namespace && location.path.startsWith(prefix.path)
     }
+
+    override protected def buildDstLocation(
+      srcPrefix: ObjectLocationPrefix,
+      dstPrefix: ObjectLocationPrefix,
+      srcLocation: ObjectLocation
+    ): ObjectLocation =
+      dstPrefix.asLocation(
+        srcLocation.path.stripPrefix(srcPrefix.path)
+      )
   }
 
   override def withPrefixTransfer[R](srcStore: MemoryRecordStore, dstStore: MemoryRecordStore)(testWith: TestWith[PrefixTransfer[ObjectLocationPrefix, ObjectLocation, ObjectLocationPrefix, ObjectLocation], R]): R =
