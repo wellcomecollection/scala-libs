432d0dfedef67e4733ac5040dac4a5455e26f02e
alicefuzier <alicefuzier@gmail.com>
2020-09-15 14:00:08 +0100

    Retry timeouts from AZure

--- storage/src/main/scala/uk/ac/wellcome/storage/azure/AzureStorageErrors.scala
+++ storage/src/main/scala/uk/ac/wellcome/storage/azure/AzureStorageErrors.scala
@@ -1,12 +1,14 @@
 package uk.ac.wellcome.storage.azure
 
 import com.azure.storage.blob.models.BlobStorageException
-import uk.ac.wellcome.storage.{DoesNotExistError, ReadError, StoreReadError}
+import uk.ac.wellcome.storage.{DoesNotExistError, ReadError, RetryableError, StoreReadError}
 
 object AzureStorageErrors {
   val readErrors: PartialFunction[Throwable, ReadError] = {
     case exc: BlobStorageException if exc.getStatusCode == 404 =>
       DoesNotExistError(exc)
+    case exc if exc.getMessage.contains("TimeoutException") =>
+      new StoreReadError(exc) with RetryableError
     case exc =>
       StoreReadError(exc)
   }
