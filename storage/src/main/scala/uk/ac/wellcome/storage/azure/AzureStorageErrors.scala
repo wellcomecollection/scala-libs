10669945aa57af0fd0f0f971ef937e7c2d69fdf2
alicefuzier <alicefuzier@gmail.com>
2020-09-16 10:03:33 +0100

    Comment

--- storage/src/main/scala/uk/ac/wellcome/storage/azure/AzureStorageErrors.scala
+++ storage/src/main/scala/uk/ac/wellcome/storage/azure/AzureStorageErrors.scala
@@ -8,6 +8,9 @@ object AzureStorageErrors {
     case exc: BlobStorageException if exc.getStatusCode == 404 =>
       DoesNotExistError(exc)
     case exc if exc.getMessage.contains("TimeoutException") =>
+      // Timeout errors from Azure should be retried and are in the form
+      // "reactor.core.Exceptions$ReactiveException: java.util.concurrent.TimeoutException: Did not
+      // observe any item or terminal signal within 60000ms in 'map' (and no fallback has been configured)"
       new StoreReadError(exc) with RetryableError
     case exc =>
       StoreReadError(exc)
