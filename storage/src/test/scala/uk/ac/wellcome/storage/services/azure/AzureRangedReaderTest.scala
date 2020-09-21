5e46eea46fce202c9902e664b678ba03bd269b7b
alicefuzier <alicefuzier@gmail.com>
2020-09-21 14:35:44 +0100

    Add RangedReader

---
+++ storage/src/test/scala/uk/ac/wellcome/storage/services/azure/AzureRangedReaderTest.scala
@@ -0,0 +1,31 @@
+package uk.ac.wellcome.storage.services.azure
+
+import uk.ac.wellcome.fixtures.TestWith
+import uk.ac.wellcome.storage.azure.AzureBlobLocation
+import uk.ac.wellcome.storage.fixtures.AzureFixtures
+import uk.ac.wellcome.storage.fixtures.AzureFixtures.Container
+import uk.ac.wellcome.storage.services.{RangedReader, RangedReaderTestCases}
+import uk.ac.wellcome.storage.store.azure.AzureTypedStore
+
+class AzureRangedReaderTest
+    extends RangedReaderTestCases[AzureBlobLocation, Container]
+    with AzureFixtures {
+  override def withNamespace[R](testWith: TestWith[Container, R]): R =
+    withAzureContainer { container =>
+      testWith(container)
+    }
+
+  override def createIdentWith(container: Container): AzureBlobLocation =
+    createAzureBlobLocationWith(container)
+
+  override def writeString(
+    location: AzureBlobLocation,
+    contents: String
+  ): Unit =
+    AzureTypedStore[String].put(location)(contents).right.value
+
+  override def withRangedReader[R](
+    testWith: TestWith[RangedReader[AzureBlobLocation], R]
+  ): R =
+    testWith(new AzureRangedReader())
+}
