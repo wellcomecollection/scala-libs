eda18ef261ba7e2b2781a0d11f927e4d5f8735d5
Alex Chan <a.chan@wellcome.ac.uk>
2020-10-13 07:52:22 +0100

    Remove an unnecessary Logging trait

--- messaging/src/main/scala/uk/ac/wellcome/messaging/worker/monitoring/metrics/cloudwatch/CloudwatchMetricsMonitoringClient.scala
+++ messaging/src/main/scala/uk/ac/wellcome/messaging/worker/monitoring/metrics/cloudwatch/CloudwatchMetricsMonitoringClient.scala
@@ -1,14 +1,12 @@
 package uk.ac.wellcome.messaging.worker.monitoring.metrics.cloudwatch
 
-import grizzled.slf4j.Logging
 import uk.ac.wellcome.messaging.worker.monitoring.metrics.MetricsMonitoringClient
 import uk.ac.wellcome.monitoring.cloudwatch.CloudWatchMetrics
 
 import scala.concurrent.Future
 
 class CloudwatchMetricsMonitoringClient(metricsSender: CloudWatchMetrics)
-    extends MetricsMonitoringClient
-    with Logging {
+    extends MetricsMonitoringClient {
 
   override def incrementCount(metricName: String): Future[Unit] =
     metricsSender.incrementCount(metricName)
