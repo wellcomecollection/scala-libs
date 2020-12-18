3b1d485465260bb3823be196ebcd3bbf727b7245
Alex Chan <alex@alexwlchan.net>
2020-12-18 09:22:06 +0000

    Remove some deprecated EnrichConfig helpers
    These were deprecated six months ago, and are no longer used anywhere in
    the platform.

--- typesafe_app/src/main/scala/uk/ac/wellcome/typesafe/config/builders/EnrichConfig.scala
+++ typesafe_app/src/main/scala/uk/ac/wellcome/typesafe/config/builders/EnrichConfig.scala
@@ -54,40 +54,5 @@ object EnrichConfig {
         )
       }
     }
-
-    @deprecated(
-      message =
-        "This method may not work as expected and will be removed in a coming release!",
-      since = "19 Jun 2020"
-    )
-    def get[T](path: String): Option[T] =
-      if (underlying.hasPath(cleanUpPath(path))) {
-        Some(underlying.getAnyRef(cleanUpPath(path)).asInstanceOf[T])
-      } else {
-        None
-      }
-
-    @deprecated(
-      message =
-        "This method may not work as expected and will be removed in a coming release!",
-      since = "19 Jun 2020"
-    ) def required[T](path: String): T =
-      get(path).getOrElse {
-
-        // For some reason merely throwing an exception here doesn't cause the
-        // app to exit, so a config failure just sits.  This causes a config
-        // error to crash the app.
-        println(s"No value found for path ${cleanUpPath(path)}")
-        System.exit(1)
-        throw new RuntimeException(
-          s"No value found for path ${cleanUpPath(path)}")
-      }
-
-    @deprecated(
-      message =
-        "This method may not work as expected and will be removed in a coming release!",
-      since = "19 Jun 2020"
-    ) def getOrElse[T](path: String)(default: T): T =
-      get(path).getOrElse(default)
   }
 }
