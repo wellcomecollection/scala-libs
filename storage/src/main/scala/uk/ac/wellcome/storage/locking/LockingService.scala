eb325e7509dcbda61ba0b34ad9a962d6a964e6f8
Alex Chan <alex@alexwlchan.net>
2021-03-22 08:43:51 +0000

    Use tail recursion to acquire only-known-good locks

--- storage/src/main/scala/uk/ac/wellcome/storage/locking/LockingService.scala
+++ storage/src/main/scala/uk/ac/wellcome/storage/locking/LockingService.scala
@@ -4,6 +4,7 @@ import cats._
 import cats.data._
 import grizzled.slf4j.Logging
 
+import scala.annotation.tailrec
 import scala.language.higherKinds
 
 trait LockingService[Out, OutMonad[_], LockDaoImpl <: LockDao[_, _]]
@@ -60,58 +61,31 @@ trait LockingService[Out, OutMonad[_], LockDaoImpl <: LockDao[_, _]]
     * unlock the entire context and report a failure.
     *
     */
+  @tailrec
   private def getLocks(ids: Set[lockDao.Ident],
-                       contextId: lockDao.ContextId): LockingServiceResult = {
-
+                       contextId: lockDao.ContextId): LockingServiceResult =
     // We lock the IDs one-by-one, but if any ID fails to lock, we skip
     // even trying to lock the remaining IDs.
     //
     // This reduces the amount of churn in the LockDao: we're skipping creating
     // and deleting a lock we know will never be used.
-    val lockResults =
-      ids.foldLeft(Set[lockDao.LockResult]())(
-        (existingResults: Set[lockDao.LockResult], id) => {
-          val failedIds = getFailedLocks(existingResults).map { _.id }
-
-          if (failedIds.isEmpty) {
-            existingResults ++ Set(lockDao.lock(id, contextId))
-          } else {
-            existingResults ++ Set(
-              Left(
-                LockFailure(
-                  id,
-                  e = new Throwable(
-                    s"Skipping locking $id; other IDs have already failed to lock"
-                  ))
-              )
-            )
-          }
-        }
-      )
-
-    assert(
-      lockResults.size == ids.size,
-      s"Got the wrong number of lockResults: size($lockResults) != size($ids)"
-    )
-
-    val failedLocks = getFailedLocks(lockResults)
-
-    if (failedLocks.isEmpty) {
+    if (ids.isEmpty) {
       Right(contextId)
     } else {
-      unlock(contextId)
-      Left(FailedLock(contextId, failedLocks))
-    }
-  }
-
-  private def getFailedLocks(
-    lockResults: Set[lockDao.LockResult]): Set[LockFailure[lockDao.Ident]] =
-    lockResults.foldLeft(Set.empty[LockFailure[lockDao.Ident]]) { (acc, o) =>
-      o match {
-        case Right(_)         => acc
-        case Left(failedLock) => acc + failedLock
+      lockDao.lock(ids.head, contextId) match {
+        case Left(failure) =>
+          Left(FailedLock(contextId, ids.tail.map(skipped) + failure))
+        case Right(_) => getLocks(ids.tail, contextId)
       }
     }
+
+  private def skipped[Ident](id: Ident): LockFailure[Ident] =
+    LockFailure(
+      id,
+      e = new Throwable(
+        s"Skipping locking $id; other IDs have already failed to lock"
+      )
+    )
 
   private def unlock(contextId: lockDao.ContextId): Unit =
     lockDao
