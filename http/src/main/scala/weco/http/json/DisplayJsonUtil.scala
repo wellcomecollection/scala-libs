e39bb6e070e2194f4fa919db7c245cc8b105ad73
Alex Chan <alex@alexwlchan.net>
2021-04-26 16:11:38 +0100

    Bring across the DisplayJsonUtil from the catalogue API

---
+++ http/src/main/scala/weco/http/json/DisplayJsonUtil.scala
@@ -0,0 +1,29 @@
+package weco.http.json
+
+import io.circe.generic.extras.{AutoDerivation, Configuration}
+import io.circe.{Encoder, Printer}
+import io.circe.syntax._
+
+/** Format JSON objects as suitable for display.
+  *
+  * This is different from `JsonUtil` for a couple of reasons:
+  *
+  *   - We enable autoderivation for a smaller number of types.  We should never
+  *     see `URL` or `UUID` in our display models, so we don't have encoders here.
+  *   - We omit null values, but display empty lists.  For internal apps, we always
+  *     render the complete value for disambiguation.
+  *
+  */
+object DisplayJsonUtil extends AutoDerivation {
+  val printer: Printer = Printer.noSpaces.copy(
+    dropNullValues = true
+  )
+
+  implicit val customConfig: Configuration =
+    Configuration.default.withDefaults
+
+  def toJson[T](value: T)(implicit encoder: Encoder[T]): String = {
+    assert(encoder != null)
+    printer.print(value.asJson)
+  }
+}
---
+++ http/src/test/scala/weco/http/models/DisplayErrorTest.scala
@@ -0,0 +1,44 @@
+package weco.http.models
+
+import akka.http.scaladsl.model.StatusCodes
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import uk.ac.wellcome.json.utils.JsonAssertions
+import weco.http.json.DisplayJsonUtil._
+
+class DisplayErrorTest extends AnyFunSpec with Matchers with JsonAssertions {
+  import DisplayError._
+
+  it("serialises to JSON without a description") {
+    val error = DisplayError(statusCode = StatusCodes.InternalServerError)
+
+    assertJsonStringsAreEqual(
+      toJson(error),
+      s"""
+         |{
+         |  "errorType": "http",
+         |  "httpStatus": 500,
+         |  "label": "Internal Server Error",
+         |  "type": "Error"
+         |}
+         |""".stripMargin
+    )
+  }
+
+  it("serialises to JSON with a description") {
+    val error = DisplayError(statusCode = StatusCodes.NotFound, description = "I couldn't find that!")
+
+    assertJsonStringsAreEqual(
+      toJson(error),
+      s"""
+         |{
+         |  "errorType": "http",
+         |  "httpStatus": 404,
+         |  "label": "Not Found",
+         |  "description": "I couldn't find that!",
+         |  "type": "Error"
+         |}
+         |""".stripMargin
+    )
+  }
+}
