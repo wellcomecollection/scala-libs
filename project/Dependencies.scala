469f991ba3158e43d825a72e169b0ec17999ccde
Alex Chan <alex@alexwlchan.net>
2021-04-26 16:00:28 +0100

    Set up an initial http project

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,16 +17,44 @@ object Dependencies {
 
     val circe = "0.13.0"
     val typesafe = "1.3.2"
+    val jackson = "2.12.3"
+    val javaxWebServicesRestful = "2.1.1"
     val logback = "1.1.8"
     val mockito = "1.10.19"
     val scalatest = "3.2.3"
     val scalatestPlus = "3.1.2.0"
     val scalatestPlusMockitoArtifactId = "mockito-3-2"
     val scanamo = "1.0-M13"
+    val swagger = "2.1.9"
     val apacheCommons = "2.6"
 
     // Provides slf4j-api
     val grizzled = "1.3.2"
+
+    // Getting the akka-http dependencies right can be fiddly and takes some work.
+    // In particular you need to use the same version of akka-http everywhere, or you
+    // get errors (from LargeResponsesTest) like:
+    //
+    //      Detected possible incompatible versions on the classpath. Please note that
+    //      a given Akka HTTP version MUST be the same across all modules of Akka HTTP
+    //      that you are using, e.g. if you use [10.1.10] all other modules that are
+    //      released together MUST be of the same version.
+    //
+    //      Make sure you're using a compatible set of libraries.
+    //
+    // To work this out:
+    //
+    //   1. Look at the version of alpakka-streams used above.
+    //
+    //   2. Look at the corresponding akka-http dependency in alpakka:
+    //      https://github.com/akka/alpakka/blob/master/project/Dependencies.scala
+    //      (At time of writing, alpakka v1.1.2 pulls in akka-http 10.1.10)
+    //
+    //   3. Look at versions of akka-http-json.  Browse the Git tags until you find
+    //      one that uses the same version of akka-http and a compatible Circe:
+    //      https://github.com/hseeberger/akka-http-json/blob/master/build.sbt
+    //
+    val akkaHttp = "10.1.10"
   }
 
   val circeDependencies = Seq(
@@ -70,6 +98,10 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-slf4j" % versions.akka
   )
 
+  val akkaHttpDependencies = Seq(
+    "com.typesafe.akka" %% "akka-http" % versions.akkaHttp
+  )
+
   val apacheCommons = Seq(
     "commons-io" % "commons-io" % versions.apacheCommons,
     "commons-io" % "commons-io" % versions.apacheCommons % "test"
@@ -87,6 +119,16 @@ object Dependencies {
   val elasticApmBridgeDependencies = Seq(
     "co.elastic.apm" % "apm-opentracing" % versions.elasticApm,
     "co.elastic.apm" % "apm-agent-attach" % versions.elasticApm
+  )
+
+  val swaggerDependencies = Seq(
+    "io.swagger.core.v3" % "swagger-core" % versions.swagger,
+    "io.swagger.core.v3" % "swagger-annotations" % versions.swagger,
+    "io.swagger.core.v3" % "swagger-models" % versions.swagger,
+    "io.swagger.core.v3" % "swagger-integration" % versions.swagger,
+    "io.swagger.core.v3" % "swagger-jaxrs2" % versions.swagger,
+    "javax.ws.rs" % "javax.ws.rs-api" % versions.javaxWebServicesRestful,
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % versions.jackson
   )
 
   val monitoringDependencies = Seq(
@@ -128,4 +170,7 @@ object Dependencies {
   ) ++
     scanamoDependencies ++
     apacheCommons
+
+  val httpDependencies: Seq[ModuleID] =
+    swaggerDependencies ++ akkaHttpDependencies
 }
