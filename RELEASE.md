d6cfd195434eb3c5d373ad4065b04afb5347fe16
Alex Chan <a.chan@wellcome.org>
2021-05-07 08:59:57 +0100

    Force people to supply a host/username/password
    An Elasticsearch client isn't useful without those things, so hard fail
    on startup if they aren't supplied.

---
+++ RELEASE.md
@@ -0,0 +1,3 @@
+RELEASE_TYPE: patch
+
+The elasticsearch_typesafe library now requires that you configure at least a host, username and password.
--- elasticsearch_typesafe/src/main/scala/uk/ac/wellcome/elasticsearch/typesafe/ElasticBuilder.scala
+++ elasticsearch_typesafe/src/main/scala/uk/ac/wellcome/elasticsearch/typesafe/ElasticBuilder.scala
@@ -8,21 +8,16 @@ import uk.ac.wellcome.typesafe.config.builders.EnrichConfig._
 object ElasticBuilder {
   def buildElasticClient(config: Config,
                          namespace: String = ""): ElasticClient = {
-    val hostname = config
-      .getStringOption(s"es.$namespace.host")
-      .getOrElse("localhost")
+    val hostname = config.requireString(s"es.$namespace.host")
+    val username = config.requireString(s"es.$namespace.username")
+    val password = config.requireString(s"es.$namespace.password")
+
     val port = config
       .getIntOption(s"es.$namespace.port")
       .getOrElse(9200)
     val protocol = config
       .getStringOption(s"es.$namespace.protocol")
       .getOrElse("http")
-    val username = config
-      .getStringOption(s"es.$namespace.username")
-      .getOrElse("username")
-    val password = config
-      .getStringOption(s"es.$namespace.password")
-      .getOrElse("password")
     val compressionEnabled = config
       .getBooleanOption(s"es.$namespace.compressionEnabled")
       .getOrElse(false)
