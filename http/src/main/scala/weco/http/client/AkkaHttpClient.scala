9fc75ab5916fc8489cde576244447b77a4a70bd7
Alex Chan <a.chan@wellcome.org>
2021-06-11 07:07:13 +0100

    Log requests in AkkaHttpClient

--- http/src/main/scala/weco/http/client/AkkaHttpClient.scala
+++ http/src/main/scala/weco/http/client/AkkaHttpClient.scala
@@ -3,12 +3,15 @@ package weco.http.client
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import grizzled.slf4j.Logging
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class AkkaHttpClient(implicit system: ActorSystem,
                      val ec: ExecutionContext)
-    extends HttpClient {
-  override def singleRequest(request: HttpRequest): Future[HttpResponse] =
+    extends HttpClient with Logging {
+  override def singleRequest(request: HttpRequest): Future[HttpResponse] = {
+    debug(s"About to send request $request")
     Http().singleRequest(request)
+  }
 }
