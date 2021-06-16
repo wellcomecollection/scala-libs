786286d10d81092f76b4d8fc2ccc70348bc2718f
Alex Chan <a.chan@wellcome.org>
2021-06-16 09:31:11 +0100

    Now actually create the branch and push it to the repos

--- .buildkite/scripts/open_downstream_prs.py
+++ .buildkite/scripts/open_downstream_prs.py
@@ -1,16 +1,52 @@
 #!/usr/bin/env python
 
+import contextlib
 import os
 import tempfile
 
 from commands import git
 
 
+@contextlib.contextmanager
+def working_directory(path):
+    """
+    Changes the working directory to the given path, then returns to the
+    original directory when done.
+    """
+    prev_cwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(prev_cwd)
+
+
+def update_scala_libs_version(new_version):
+    old_lines = list(open("project/Dependencies.scala"))
+
+    with open("project/Dependencies.scala", "w") as out_file:
+        for line in old_lines:
+            if line.startswith("    val defaultVersion"):
+                out_file.write(f'    val defaultVersion = "{new_version}"\n')
+            else:
+                out_file.write(line)
+
+
 if __name__ == '__main__':
     new_version = "v26.18.0"
 
-    for repo in ("catalogue-api", "pipeline", "storage-service"):
-        tmp_dir = tempfile.mkdtemp()
+    for repo in ("catalogue-api", "catalogue-pipeline", "storage-service"):
+        repo_dir = tempfile.mkdtemp()
 
-        git("clone", f"git@github.com:wellcomecollection/{repo}.git", tmp_dir)
+        git("clone", f"git@github.com:wellcomecollection/{repo}.git", repo_dir)
+
+        with working_directory(repo_dir):
+            update_scala_libs_version(new_version)
+
+            branch_name = f"bump-scala-libs-to-{new_version}"
+            git("checkout", "-b", branch_name)
+            git("add", "project/Dependencies.scala")
+            git("commit", "-m", f"Bump scala-libs to {new_version}")
+            git("push", "origin", branch_name)
+
         print(os.listdir(tmp_dir))
