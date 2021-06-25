dfa6aff17f33d95d6aa5dfc9eb7a5edbe2919737
Alex Chan <a.chan@wellcome.org>
2021-06-25 07:26:13 +0100

    Allow setting the DynamoLockDao expiryTime via config parameters

--- storage_typesafe/src/main/scala/uk/ac/wellcome/storage/typesafe/DynamoLockDaoBuilder.scala
+++ storage_typesafe/src/main/scala/uk/ac/wellcome/storage/typesafe/DynamoLockDaoBuilder.scala
@@ -6,8 +6,10 @@ import uk.ac.wellcome.storage.locking.dynamo.{
   DynamoLockDaoConfig
 }
 import uk.ac.wellcome.typesafe.config.builders.AWSClientConfigBuilder
+import uk.ac.wellcome.typesafe.config.builders.EnrichConfig._
 
 import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
 
 object DynamoLockDaoBuilder extends AWSClientConfigBuilder {
   def buildDynamoLockDao(config: Config, namespace: String = "locking")(
@@ -16,6 +18,11 @@ object DynamoLockDaoBuilder extends AWSClientConfigBuilder {
   ) = new DynamoLockDao(
     client = DynamoBuilder.buildDynamoClient(config),
     config =
-      DynamoLockDaoConfig(DynamoBuilder.buildDynamoConfig(config, namespace))
+      DynamoLockDaoConfig(
+        dynamoConfig = DynamoBuilder.buildDynamoConfig(config, namespace),
+        expiryTime = config
+          .getDurationOption(s"aws.$namespace.dynamo.lockExpiryTime")
+          .getOrElse(3.minutes)
+      )
   )
 }
