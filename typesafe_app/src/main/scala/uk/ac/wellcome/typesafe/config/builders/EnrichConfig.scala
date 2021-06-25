6c59229ecadace66654857daba9401bf56bd9e16
Alex Chan <a.chan@wellcome.org>
2021-06-25 07:18:47 +0100

    Add EnrichConfig helpers for getting a duration

--- typesafe_app/src/main/scala/uk/ac/wellcome/typesafe/config/builders/EnrichConfig.scala
+++ typesafe_app/src/main/scala/uk/ac/wellcome/typesafe/config/builders/EnrichConfig.scala
@@ -2,6 +2,8 @@ package uk.ac.wellcome.typesafe.config.builders
 
 import com.typesafe.config.Config
 
+import scala.concurrent.duration.Duration
+
 object EnrichConfig {
 
   implicit class RichConfig(val underlying: Config) extends AnyVal {
@@ -54,6 +56,26 @@ object EnrichConfig {
     def getBooleanOption(path: String): Option[Boolean] =
       getPathValue(path) {
         underlying.getBoolean
+      }
+
+    def getDurationOption(path: String): Option[Duration] =
+      getPathValue(path) { s =>
+        // The getDuration method returns a Java Duration, but we want a
+        // Scala duration.
+        //
+        // TODO: When we upgrade to Scala 2.13, we can use the built-in converters
+        // rather than going via the number of nanoseconds.
+        // See https://stackoverflow.com/a/55412743/1558022
+        val nanos = underlying.getDuration(s).toNanos
+        Duration.fromNanos(nanos)
+      }
+
+    def requireDuration(path: String): Duration =
+      getDurationOption(path) getOrElse {
+        emergencyStop(path)
+        throw new Throwable(
+          s"No value found for path ${cleanUpPath(path)}"
+        )
       }
   }
 }
