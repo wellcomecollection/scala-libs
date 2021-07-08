96ada35f20400f79c724f45140eab5137726c00f
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2021-07-08 12:39:55 +0000

    Apply auto-formatting rules

--- typesafe_app/src/main/scala/weco/typesafe/WellcomeTypesafeApp.scala
+++ typesafe_app/src/main/scala/weco/typesafe/WellcomeTypesafeApp.scala
@@ -6,25 +6,26 @@ import grizzled.slf4j.Logging
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
-trait WellcomeTypesafeApp extends App with Logging  {
+trait WellcomeTypesafeApp extends App with Logging {
   protected def run(service: Runnable) =
     Await.result(service.run(), Duration.Inf)
 
   protected def exit(statusCode: Int = 0): Unit =
     System.exit(statusCode)
 
-  def runWithConfig(builder: Config => Runnable) = try {
-    info(s"Starting service")
+  def runWithConfig(builder: Config => Runnable) =
+    try {
+      info(s"Starting service")
 
-    run {
-      builder(ConfigFactory.load)
+      run {
+        builder(ConfigFactory.load)
+      }
+
+      info(s"Shutting down service")
+      exit(0)
+    } catch {
+      case e: Throwable =>
+        error("Fatal error, terminating service:", e)
+        exit(1)
     }
-
-    info(s"Shutting down service")
-    exit(0)
-  } catch {
-    case e: Throwable =>
-      error("Fatal error, terminating service:", e)
-      exit(1)
-  }
 }
