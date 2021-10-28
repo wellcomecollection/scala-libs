5c2f41b48da0b770e8cc3e3be7f493b1e0b49a58
Alex Chan <a.chan@wellcome.org>
2021-10-28 09:38:10 +0100

    Continue expanding the HttpLogger tests

--- http/src/main/scala/weco/http/monitoring/WellcomeHttpLogger.scala
+++ http/src/main/scala/weco/http/monitoring/WellcomeHttpLogger.scala
@@ -1,6 +1,6 @@
 package weco.http.monitoring
 
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpRequest, HttpResponse}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpHeader, HttpRequest, HttpResponse}
 import akka.http.scaladsl.server.RouteResult.Complete
 
 /** Constructs the log entries for every HTTP request/response pair.
@@ -18,22 +18,32 @@ class WellcomeHttpLogger(
   responseHeaders: Set[String]
 ) {
   def createLogLine(request: HttpRequest, response: Any): String = {
-    s"${createRequestLog(request)} / ${createResponseLog(response)}"
+    val requestLog = createLogHalf(
+      prefix = s"Request: ${request.method.value} ${request.uri.toRelative}",
+      headers = request.headers,
+      allowedHeaderNames = requestHeaders,
+      entity = request.entity
+    )
+
+    s"$requestLog / ${createResponseLog(response)}"
   }
 
-  private def createRequestLog(request: HttpRequest): String = {
-    val prefix = s"Request: ${request.method.value} ${request.uri.toRelative}"
-
+  private def createLogHalf(
+    prefix: String,
+    headers: Seq[HttpHeader],
+    allowedHeaderNames: Set[String],
+    entity: HttpEntity
+  ): String = {
     val loggedHeaders =
-      request.headers
-        .filter { h => requestHeaders.contains(h.name) }
+      headers
+        .filter { h => allowedHeaderNames.contains(h.name) }
         .map { h => s"; ${h.name}=${h.value}"}
 
     // We only log the entity ("body") if it contains anything interesting.  If the entity
     // is empty (say, on GET requests), it's just noise in the logs.
-    val entityLog = request.entity match {
+    val entityLog = entity match {
       case h: HttpEntity.Strict if h.contentType == ContentTypes.NoContentType && h.contentLength == 0 => ""
-      case _ => s"; ${request.entity}"
+      case _ => s"; $entity"
     }
 
     s"$prefix${loggedHeaders.mkString(" ")}$entityLog".trim
@@ -42,20 +52,12 @@ class WellcomeHttpLogger(
   private def createResponseLog(response: Any): String =
     response match {
       case Complete(resp: HttpResponse) =>
-        val contentLength = resp.entity.contentLengthOption match {
-          case Some(length) => s"$length bytes total"
-          case None         => "<unknown>"
-        }
-
-        val loggedHeaders =
-          resp.headers
-            .filter { h => responseHeaders.contains(h.name) }
-            .map { h => s"; ${h.name}=${h.value}"}
-
-        s"Response: HTTP ${resp.status}; " +
-          s"Content-Type=${resp.entity.contentType}; " +
-          s"Content-Length=$contentLength" +
-          loggedHeaders.mkString(" ").trim
+        createLogHalf(
+          prefix = s"Response: HTTP ${resp.status}",
+          headers = resp.headers,
+          allowedHeaderNames = responseHeaders,
+          entity = resp.entity
+        )
 
       case other => s"Response: $other"
     }
--- http/src/test/scala/weco/http/monitoring/WellcomeHttpLoggerTest.scala
+++ http/src/test/scala/weco/http/monitoring/WellcomeHttpLoggerTest.scala
@@ -1,7 +1,8 @@
 package weco.http.monitoring
 
 import akka.http.scaladsl.model.headers.{Authorization, BasicHttpCredentials, ETag}
-import akka.http.scaladsl.model.{HttpEntity, HttpMethods, HttpRequest, Uri}
+import akka.http.scaladsl.model.{HttpEntity, HttpMethods, HttpRequest, HttpResponse, StatusCodes, Uri}
+import akka.http.scaladsl.server.RouteResult.Complete
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -69,6 +70,78 @@ class WellcomeHttpLoggerTest extends AnyFunSpec with Matchers {
       )
 
       log shouldBe """Request: GET /api/v1/shapes; ETag="123456789"; HttpEntity.Strict(text/plain; charset=UTF-8,17 bytes total) / Response: <unknown>"""
+    }
+  }
+
+  describe("logging the response") {
+    it("doesn't log any headers if none are approved") {
+      val logger = new WellcomeHttpLogger(
+        requestHeaders = Set(),
+        responseHeaders = Set()
+      )
+
+      val log = logger.createLogLine(
+        request = HttpRequest(
+          method = HttpMethods.GET,
+          uri = Uri("http://example.net/api/v1/shapes"),
+        ),
+        response = Complete(HttpResponse(
+          status = StatusCodes.OK,
+          headers = List(
+            ETag("123456789"),
+            Authorization(BasicHttpCredentials("secretValue")),
+          )
+        ))
+      )
+
+      log shouldBe """Request: GET /api/v1/shapes / Response: HTTP 200 OK"""
+    }
+
+    it("only logs the approved headers") {
+      val logger = new WellcomeHttpLogger(
+        requestHeaders = Set(),
+        responseHeaders = Set("ETag")
+      )
+
+      val log = logger.createLogLine(
+        request = HttpRequest(
+          method = HttpMethods.GET,
+          uri = Uri("http://example.net/api/v1/shapes"),
+        ),
+        response = Complete(HttpResponse(
+          status = StatusCodes.OK,
+          headers = List(
+            ETag("123456789"),
+            Authorization(BasicHttpCredentials("secretValue")),
+          )
+        ))
+      )
+
+      log shouldBe """Request: GET /api/v1/shapes / Response: HTTP 200 OK; ETag="123456789""""
+    }
+
+    it("logs a non-trivial entity, if present") {
+      val logger = new WellcomeHttpLogger(
+        requestHeaders = Set(),
+        responseHeaders = Set("ETag")
+      )
+
+      val log = logger.createLogLine(
+        request = HttpRequest(
+          method = HttpMethods.GET,
+          uri = Uri("http://example.net/api/v1/shapes"),
+        ),
+        response = Complete(HttpResponse(
+          status = StatusCodes.OK,
+          headers = List(
+            ETag("123456789"),
+            Authorization(BasicHttpCredentials("secretValue")),
+          ),
+          entity = HttpEntity("this is the response body")
+        ))
+      )
+
+      log shouldBe """Request: GET /api/v1/shapes / Response: HTTP 200 OK; ETag="123456789"; HttpEntity.Strict(text/plain; charset=UTF-8,25 bytes total)"""
     }
   }
 }
