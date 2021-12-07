502a1d93e1252fcf16307d800b8d123ba238f5d9
Alex Chan <a.chan@wellcome.org>
2021-12-07 16:25:52 +0000

    try to get buildkite working

--- .buildkite/scripts/publish.py
+++ .buildkite/scripts/publish.py
@@ -1,9 +1,6 @@
-# -*- encoding: utf-8
-
-import os
 import sys
 
-from commands import sbt, git
+from commands import sbt, git, run_build_script
 from git_utils import remote_default_branch
 from release import has_release
 
@@ -11,7 +8,7 @@ def publish(project_name):
     if has_release():
         print(f"Release detected, publishing {project_name}.")
         git("pull", "origin", remote_default_branch())
-        sbt(f"project {project_name}", "publish")
+        run_build_script("run_sbt_task_in_docker.sh", f"project {project_name}", "publish")
     else:
         print("No release detected, exit gracefully.")
         sys.exit(0)
@@ -21,6 +18,6 @@ def publish(project_name):
 # when used with the buildkite docker plugin incorrectly parses
 # spaces as newlines preventing passing args to this script!
 if __name__ == '__main__':
-    project = os.environ["PROJECT"]
+    project = sys.argv[1]
 
     publish(project)
