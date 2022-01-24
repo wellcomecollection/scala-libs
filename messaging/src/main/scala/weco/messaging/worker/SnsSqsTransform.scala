f448f26f560f8120c6728c93c25c33a77dfb090c
Alex Chan <a.chan@wellcome.org>
2022-01-24 12:06:04 +0000

    Remove a now-unused type

--- messaging/src/main/scala/weco/messaging/worker/SnsSqsTransform.scala
+++ messaging/src/main/scala/weco/messaging/worker/SnsSqsTransform.scala
@@ -6,8 +6,6 @@ import weco.json.JsonUtil.fromJson
 import weco.messaging.sns.NotificationMessage
 
 trait SnsSqsTransform[Work] {
-  type SQSTransform = SQSMessage => Either[Throwable, Work]
-
   implicit val wd: Decoder[Work]
 
   val parseMessage = (message: SQSMessage) => {
