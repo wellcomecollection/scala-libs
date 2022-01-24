77bebee8421c1d14565f121866001551dc1a0878
Alex Chan <a.chan@wellcome.org>
2022-01-24 15:50:45 +0000

    Emphasise that this is the caller's opinion

--- messaging/src/main/scala/weco/messaging/worker/models/Result.scala
+++ messaging/src/main/scala/weco/messaging/worker/models/Result.scala
@@ -10,7 +10,7 @@ sealed trait Result[Summary] {
     s"$name: ${summary.getOrElse("<no-summary>")}"
 }
 
-// An operation that failed, and can't be retried.
+// An operation that failed, and which the caller thinks can't be retried.
 //
 // e.g. trying to write an object to a non-existent S3 bucket.  This will always
 // fail, no matter how often we try.
@@ -19,7 +19,7 @@ case class TerminalFailure[Summary](
   summary: Option[Summary] = Option.empty[Summary]
 ) extends Result[Summary]
 
-// An operation that failed, and can be retried.
+// An operation that failed, and which the caller knows can be retried.
 //
 // e.g. a 500 error when trying to write an object to S3.  This will usually
 // succeed if we try again after a short pause.
