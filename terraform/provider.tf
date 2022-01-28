be97315da0dfcc35ff81172d9026ef0b99a296da
Alex Chan <a.chan@wellcome.org>
2022-01-28 12:08:42 +0000

    Add some default tags

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,4 +4,14 @@ provider "aws" {
   assume_role {
     role_arn = "arn:aws:iam::760097843905:role/platform-admin"
   }
+
+  default_tags {
+    tags = {
+      TerraformConfigurationURL = "https://github.com/wellcomecollection/scala-libs/tree/main/terraform"
+      Environment               = "Production"
+      Department                = "Digital Platform"
+      Division                  = "Wellcome Collection"
+      Use                       = "scala-libs release bucket"
+    }
+  }
 }
