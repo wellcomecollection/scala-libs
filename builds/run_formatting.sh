07d8f5ba37b461d9d03a061943d419fc2a123f9f
Alex Chan <a.chan@wellcome.org>
2022-11-28 14:22:06 +0000

    simplify the "run formatting" script

--- builds/run_formatting.sh
+++ builds/run_formatting.sh
@@ -2,26 +2,5 @@
 
 set -o errexit
 set -o nounset
-set -o verbose
 
-ECR_REGISTRY="760097843905.dkr.ecr.eu-west-1.amazonaws.com"
-
-ROOT=$(git rev-parse --show-toplevel)
-
-# Coursier cache location is platform-dependent
-# https://get-coursier.io/docs/cache.html#default-location
-LINUX_COURSIER_CACHE=".cache/coursier/v1"
-
-if [[ $(uname) == "Darwin" ]]
-then
-  HOST_COURSIER_CACHE=~/Library/Caches/Coursier/v1
-else
-  HOST_COURSIER_CACHE=~/$LINUX_COURSIER_CACHE
-fi
-
-docker run --tty --rm \
-  --volume ~/.sbt:/root/.sbt \
-  --volume ~/.ivy2:/root/.ivy2 \
-  --volume "$HOST_COURSIER_CACHE:/root/$LINUX_COURSIER_CACHE" \
-	--volume "$ROOT:/repo" \
-	"$ECR_REGISTRY/wellcome/scalafmt"
+./builds/run_sbt_task_in_docker.sh "scalafmt"
