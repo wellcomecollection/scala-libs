7ef650426e04a32977684e42b9e3d76ae53aa238
Alex Chan <a.chan@wellcome.org>
2023-01-06 13:53:16 +0000

    Improve this variable name

--- storage/src/main/scala/weco/storage/listing/s3/S3ObjectLocationListing.scala
+++ storage/src/main/scala/weco/storage/listing/s3/S3ObjectLocationListing.scala
@@ -3,10 +3,10 @@ package weco.storage.listing.s3
 import software.amazon.awssdk.services.s3.S3Client
 import weco.storage.s3.{S3ObjectLocation, S3ObjectLocationPrefix}
 
-class S3ObjectLocationListing(implicit summaryListing: S3ObjectListing)
+class S3ObjectLocationListing(implicit objectListing: S3ObjectListing)
     extends S3Listing[S3ObjectLocation] {
   override def list(prefix: S3ObjectLocationPrefix): ListingResult =
-    summaryListing.list(prefix) match {
+    objectListing.list(prefix) match {
       case Right(result) =>
         Right(result.map { s3Object =>
           S3ObjectLocation(bucket = prefix.bucket, key = s3Object.key())
