26875aa2314ac3466e89171e6bd01f46835733a0
Alex Chan <a.chan@wellcome.org>
2023-08-04 13:13:33 +0100

    Remove the `services` namespace from the storage library

---
+++ RELEASE.md
@@ -0,0 +1,3 @@
+RELEASE_TYPE: minor
+
+Remove the `services` namespace from the storage library, which is only used by the storage service.
--- storage/src/main/scala/weco/storage/services/RangedReader.scala
+++
@@ -1,9 +0,0 @@
-package weco.storage.services
-
-import weco.storage.ReadError
-import weco.storage.models.ByteRange
-import weco.storage.models.ByteRange
-
-trait RangedReader[Ident] {
-  def getBytes(id: Ident, range: ByteRange): Either[ReadError, Array[Byte]]
-}
