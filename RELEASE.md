9512606a3a07885deba07310cc9a283d41b544c9
Alex Chan <a.chan@wellcome.org>
2023-08-07 10:09:24 +0100

    Remove the listing namespace

---
+++ RELEASE.md
@@ -0,0 +1,3 @@
+RELEASE_TYPE: minor
+
+Remove the `storage.listing` namespace, which is only used by the storage service.
--- storage/src/main/scala/weco/storage/listing/Listing.scala
+++
@@ -1,9 +0,0 @@
-package weco.storage.listing
-
-import weco.storage.ListingFailure
-
-trait Listing[Prefix, Result] {
-  type ListingResult = Either[ListingFailure[Prefix], Iterable[Result]]
-
-  def list(prefix: Prefix): ListingResult
-}
