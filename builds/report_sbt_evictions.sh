7197fed88459411a5215a40f15c70f5a0747c623
Paul Butcher <paul.butcher@gmail.com>
2023-10-03 15:00:40 +0100

    allow script to actually run

--- builds/report_sbt_evictions.sh
+++ builds/report_sbt_evictions.sh
@@ -1,3 +1,4 @@
+mkdir -p .reports
 ./builds/run_sbt_task_in_docker.sh "project $1" "evicted" | grep '^\[warn\]' | tee .reports/evicted
 WARNING_COUNT=$(sed '/^$/d' | wc -l )
 if [ $WARNING_COUNT != 0 ]
