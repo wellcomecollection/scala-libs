69f7cde26b6e13a8729320ae3a96ac265050754c
Paul Butcher <paul.butcher@gmail.com>
2023-10-03 15:54:17 +0100

    improve warning detection

--- builds/report_sbt_evictions.sh
+++ builds/report_sbt_evictions.sh
@@ -1,6 +1,8 @@
 mkdir -p .reports
 ./builds/run_sbt_task_in_docker.sh "project $1" "evicted" | grep '^\[warn\]' | tee .reports/evicted
-WARNING_COUNT=$(cat .reports/evicted | sed '/^$/d' | wc -l )
+WARNING_COUNT=$(cat .reports/evicted | grep -E '^\[warn\]\s+\*'  )
 if [ $WARNING_COUNT != 0 ]
+then
+  echo "found $WARNING_COUNT suspected binary incompatible evictions"
   exit 2
 fi
