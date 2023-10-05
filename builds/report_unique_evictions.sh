5c4135ed5fee4741f9407541411b14b47d0ab5b4
Paul Butcher <paul.butcher@gmail.com>
2023-10-05 09:34:43 +0100

    add summary note

--- builds/report_unique_evictions.sh
+++ builds/report_unique_evictions.sh
@@ -1,4 +1,4 @@
-echo "# Suspected binary incompatible evictions across all projects (distinct)"
-cat .reports/evicted_* | grep -E '^\[warn\]\s+\*' | sed 's/.*\*/*/'  |sort | uniq
-echo '```'
-echo '```'
+# Intended to be run after producing eviction reports with report_sbt_evictions.sh
+echo "# Suspected binary incompatible evictions across all projects (summary)"
+cat .reports/evicted_* | grep -E '^\[warn\]\s+\*' | sed 's/.*\*/*/' | sort | uniq
+echo "See individual _evictions_ stages for more detail"
