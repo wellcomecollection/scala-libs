4216053f6a652d36320f91af3065a36a751e876f
Štěpán Brychta <stepan.brychta@me.com>
2024-09-16 09:45:55 +0100

    Edit existing eviction comment if exists #5780

---
+++ .github/workflows/post-evictions-comment.js
@@ -0,0 +1,31 @@
+const postEvictionsComment = async (github, context) => {
+    const fs = require('fs');
+    const reportContent = fs.readFileSync('unique_evictions.txt', 'utf8');
+
+    const comments = await github.rest.issues.listComments({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo
+    });
+
+    const commentMarker = 'Suspected binary incompatible evictions across all projects (summary)';
+    const existingComment = comments.data.find(comment => comment.body.includes(commentMarker));
+
+    if (existingComment) {
+        await github.rest.issues.updateComment({
+            comment_id: existingComment.id,
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            body: reportContent
+        });
+    }
+    else {
+        github.rest.issues.createComment({
+            issue_number: context.issue.number,
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            body: reportContent
+        });
+    }
+}
+
